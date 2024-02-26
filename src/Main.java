4b9d0bf11b61cb1c00f84a81cc5a1e2464134ce0
Namanis <mykolas.skrodenis@gmail.com>
2024-02-26 15:28:36 +0200

    Find North, East, South and West points

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,8 @@ public class Main {
         double[][] points = {
                 {0, 0, 0},
                 {100, 0, 0},
-                {100, 50, 0},
+                {100, 25, 0},
+                {75, 50, 0},
                 {0, 50, 0},
 
         };
@@ -49,8 +50,8 @@ public class Main {
         // Given a better thought maybe there can be more than 2 points
 
         // Find the westernmost points
-        List<Integer> extremePoints = findWest(points);
-        System.out.println("Westernmost Points:");
+        List<Integer> extremePoints = findExtreme(points, azimuthDegrees);
+        System.out.println("Extreme Points:");
         System.out.println(extremePoints.size());
 
         for (int index : extremePoints) {
@@ -59,24 +60,76 @@ public class Main {
         }
     }
 
-    //Todo find North, East, and South as well given that user inputs (0, 90, 180)
+
     //Todo for any Azimuth input
-    private static List<Integer> findWest(double[][] points) {
-        List<Integer> westIndices = new ArrayList<>();
+    //A lot of almost repetitive code!
+    private static List<Integer> findExtreme(double[][] points, double azimuthDegrees) {
+        List<Integer> extremePoints = new ArrayList<>();
         double minCoordinateX = points[0][0];
-        westIndices.add(0);
-        for (int i = 1; i < points.length; i++) {
-            double currentX = points[i][0];
-            if (currentX < minCoordinateX) {
-                minCoordinateX = currentX;
-                westIndices.clear();
-                westIndices.add(i);
-            } else if (currentX == minCoordinateX) {
-                westIndices.add(i);
+        double minCoordinateY = points[0][1];
+        double maxCoordinateX = points[0][0];
+        double maxCoordinateY = points[0][1];
+        extremePoints.add(0);
+
+        //Y must be highest
+        if(azimuthDegrees == 0)
+        {
+            for (int i = 1; i < points.length; i++) {
+                double currentY = points[i][1];
+                if (currentY > maxCoordinateY) {
+                    maxCoordinateY = currentY;
+                    extremePoints.clear();
+                    extremePoints.add(i);
+                } else if (currentY == maxCoordinateY) {
+                    extremePoints.add(i);
+                }
             }
         }
+        // X must be highest
+        else if(azimuthDegrees == 90)
+        {
+            for (int i = 1; i < points.length; i++) {
+                double currentX = points[i][0];
+                if (currentX > maxCoordinateX) {
+                    maxCoordinateX = currentX;
+                    extremePoints.clear();
+                    extremePoints.add(i);
+                } else if (currentX == maxCoordinateX) {
+                    extremePoints.add(i);
+                }
+            }
+        }
+        //Y must be lowest
+        else if(azimuthDegrees == 180)
+        {
+            for (int i = 1; i < points.length; i++) {
+                double currentY = points[i][1];
+                if (currentY < minCoordinateY) {
+                    minCoordinateY = currentY;
+                    extremePoints.clear();
+                    extremePoints.add(i);
+                } else if (currentY == minCoordinateY) {
+                    extremePoints.add(i);
+                }
+            }
+        }
+        //X must be lowest
+        else if(azimuthDegrees == 270)
+        {
+            for (int i = 1; i < points.length; i++) {
+                double currentX = points[i][0];
+                if (currentX < minCoordinateX) {
+                    minCoordinateX = currentX;
+                    extremePoints.clear();
+                    extremePoints.add(i);
+                } else if (currentX == minCoordinateX) {
+                    extremePoints.add(i);
+                }
+            }
+        }
+        else System.out.println("Not valid (for now)");
 
-        return westIndices;
+        return extremePoints;
     }
 
 }
